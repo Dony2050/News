060c75e49a09222e3b86b76e02557bd6e71fe207
Dony <dony2023@163.com>
2025-05-21 11:08:34 +0800

    add itemcf_sim and usercf_sim in main.py

--- multi_recall/main.py
+++ multi_recall/main.py
@@ -121,7 +121,7 @@ def get_user_item_time(click_df):
     def make_item_time_pair(df):
         return list(zip(df['click_article_id'], df['click_timestamp']))
 
-    user_item_time_df = click_df.groupby('user_id')['click_article_id', 'click_timestamp'].apply(
+    user_item_time_df = click_df.groupby('user_id')[['click_article_id', 'click_timestamp']].apply(
         lambda x: make_item_time_pair(x)) \
         .reset_index().rename(columns={0: 'item_time_list'})
     user_item_time_dict = dict(zip(user_item_time_df['user_id'], user_item_time_df['item_time_list']))
@@ -243,3 +243,112 @@ def metrics_recall(user_recall_items_dict, trn_last_click_df, topk=5):
         print(' topk: ', k, ' : ', 'hit_num: ', hit_num, 'hit_rate: ', hit_rate, 'user_num : ', user_num)
 
 
+# itemcf i2i_sim
+# 用户点击的时间权重
+# 用户点击的顺序权重
+# 文章创建的时间权重
+def itemcf_sim(df, item_created_time_dict):
+    """
+        文章与文章之间的相似性矩阵计算
+        :param df: 数据表
+        :item_created_time_dict:  文章创建时间的字典
+        return : 文章与文章的相似性矩阵
+
+        思路: 基于物品的协同过滤(详细请参考上一期推荐系统基础的组队学习) + 关联规则
+    """
+
+    user_item_time_dict = get_user_item_time(df)
+    '''{user1: [(item1, time1), (item2, time2), ...]}'''
+    # 计算物品相似度
+    i2i_sim = {}
+    '''{item_i: {item_j: sim_score}}'''
+    item_cnt = defaultdict(int)
+    for user, item_time_list in tqdm(user_item_time_dict.items()):
+        # 在基于商品的协同过滤优化的时候可以考虑时间因素
+        for loc1, (i, i_click_time) in enumerate(item_time_list):
+            item_cnt[i] += 1
+            i2i_sim.setdefault(i, {})
+            for loc2, (j, j_click_time) in enumerate(item_time_list):
+                if (i == j):
+                    continue
+
+                # 考虑文章的正向顺序点击和反向顺序点击
+                loc_alpha = 1.0 if loc2 > loc1 else 0.7
+                # 位置信息权重
+                loc_weight = loc_alpha * (0.9 ** (np.abs(loc2 - loc1) - 1))
+                # 点击时间权重
+                click_time_weight = np.exp(0.7 ** np.abs(i_click_time - j_click_time))
+                # 两篇文章创建时间的权重
+                created_time_weight = np.exp(0.8 ** np.abs(item_created_time_dict[i] - item_created_time_dict[j]))
+                i2i_sim[i].setdefault(j, 0)
+                # 考虑多种因素的权重计算最终的文章之间的相似度
+                i2i_sim[i][j] += loc_weight * click_time_weight * created_time_weight / math.log(
+                    len(item_time_list) + 1)
+
+    i2i_sim_ = i2i_sim.copy()
+    for i, related_items in i2i_sim.items():
+        for j, wij in related_items.items():
+            i2i_sim_[i][j] = wij / math.sqrt(item_cnt[i] * item_cnt[j])
+
+    # 将得到的相似性矩阵保存到本地
+    pickle.dump(i2i_sim_, open(save_path + 'itemcf_i2i_sim.pkl', 'wb'))
+
+    return i2i_sim_
+
+
+i2i_sim = itemcf_sim(all_click_df, item_created_time_dict)
+
+
+# usercf u2u_sim
+def get_user_activate_degree_dict(all_click_df):
+    all_click_df_ = all_click_df.groupby('user_id')['click_article_id'].count().reset_index()
+
+    # 用户活跃度归一化
+    mm = MinMaxScaler()
+    all_click_df_['click_article_id'] = mm.fit_transform(all_click_df_[['click_article_id']])
+    user_activate_degree_dict = dict(zip(all_click_df_['user_id'], all_click_df_['click_article_id']))
+
+    return user_activate_degree_dict
+
+
+def usercf_sim(all_click_df, user_activate_degree_dict):
+    """
+        用户相似性矩阵计算
+        :param all_click_df: 数据表
+        :param user_activate_degree_dict: 用户活跃度的字典
+        return 用户相似性矩阵
+
+        思路: 基于用户的协同过滤(详细请参考上一期推荐系统基础的组队学习) + 关联规则
+    """
+    item_user_time_dict = get_item_user_time_dict(all_click_df)
+    '''{item1: [(user1, timestamp1),(user2, timestamp2)...],...}'''
+    u2u_sim = {}
+    '''{user1: {user2: similarity, ...}, ...}'''
+    user_cnt = defaultdict(int)
+    for item, user_time_list in tqdm(item_user_time_dict.items()):
+        for u, click_time in user_time_list:
+            user_cnt[u] += 1
+            u2u_sim.setdefault(u, {})
+            for v, click_time in user_time_list:
+                u2u_sim[u].setdefault(v, 0)
+                if u == v:
+                    continue
+                # 用户平均活跃度作为活跃度的权重
+                activate_weight = 100 * 0.5 * (user_activate_degree_dict[u] + user_activate_degree_dict[v])
+                u2u_sim[u][v] += activate_weight / math.log(len(user_time_list) + 1)
+
+    u2u_sim_ = u2u_sim.copy()
+    for u, related_users in u2u_sim.items():
+        for v, wij in related_users.items():
+            u2u_sim_[u][v] = wij / math.sqrt(user_cnt[u] * user_cnt[v])
+
+    # 将得到的相似性矩阵保存到本地
+    pickle.dump(u2u_sim_, open(save_path + 'usercf_u2u_sim.pkl', 'wb'))
+
+    return u2u_sim_
+
+
+# 由于usercf计算时候太耗费内存了，这里就不直接运行了
+# 如果是采样的话，是可以运行的
+user_activate_degree_dict = get_user_activate_degree_dict(all_click_df)
+u2u_sim = usercf_sim(all_click_df, user_activate_degree_dict)
